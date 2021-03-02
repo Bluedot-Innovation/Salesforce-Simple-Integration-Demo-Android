efe3f7735614bf5486d888bc779d218736d6b869
Neha <neha@bluedot.io>
2021-03-02 22:36:14 +1100

    marketing cloud sdk updated to latest version

--- app/src/main/java/bluedotinnovation/com/bdsalesforceintegrationsample/MainApplication.java
+++ app/src/main/java/bluedotinnovation/com/bdsalesforceintegrationsample/MainApplication.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,20 +13,15 @@ import android.os.Build;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
-import au.com.bluedot.point.net.engine.BDError;
 import au.com.bluedot.point.net.engine.GeoTriggeringService;
-import au.com.bluedot.point.net.engine.GeoTriggeringStatusListener;
-import au.com.bluedot.point.net.engine.InitializationResultListener;
 import au.com.bluedot.point.net.engine.ServiceManager;
 import com.salesforce.marketingcloud.InitializationStatus;
 import com.salesforce.marketingcloud.MarketingCloudConfig;
 import com.salesforce.marketingcloud.MarketingCloudSdk;
 import com.salesforce.marketingcloud.notifications.NotificationCustomizationOptions;
-import com.salesforce.marketingcloud.notifications.NotificationMessage;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import org.jetbrains.annotations.Nullable;
 
 import static android.app.Notification.PRIORITY_MAX;
 
@@ -34,18 +30,19 @@ import static android.app.Notification.PRIORITY_MAX;
  * Copyright (c) 2018 Bluedot Innovation. All rights reserved.
  * MainApplication demonstrates the implementation Bluedot Point SDK and Marketing Cloud SDK.
  */
-public class MainApplication extends Application implements MarketingCloudSdk.InitializationListener {
+public class MainApplication extends Application
+        implements MarketingCloudSdk.InitializationListener {
 
     public static final String NOTIFICATION_TITLE = "Location Based Notifications";
-    public static final String NOTIFICATION_CONTENT = "--PLEASE CHANGE-- This app is utilizing the location to trigger alerts " +
-            "in both background and foreground modes when you visit your favourite locations";
+    public static final String NOTIFICATION_CONTENT =
+            "--PLEASE CHANGE-- This app is utilizing the location to trigger alerts " +
+                    "in both background and foreground modes when you visit your favourite locations";
     //=============================== [ Bluedot SDK ] ===============================
     private ServiceManager mServiceManager;
-    private String projectId = ""; //ProjectId of Bluedot App from Canvas
+    private final String projectId = "<YOUR_PROJECT_ID>"; //ProjectId of Bluedot App from Canvas
     //  
     final String CHANNEL_ID = "BluedotSampleChannelId";     //Please replace with yout Channel Id
     final String CHANNEL_NAME = "BluedotSampleChannelName"; //Please replace with your Channel Name
-
 
     @Override
     public void onCreate() {
@@ -58,28 +55,29 @@ public class MainApplication extends Application implements MarketingCloudSdk.In
 
     public void initPointSDK() {
         boolean locationPermissionGranted =
-                ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                ActivityCompat.checkSelfPermission(getApplicationContext(),
+                                                   Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED;
 
         if (locationPermissionGranted) {
             mServiceManager = ServiceManager.getInstance(this);
 
             if (!mServiceManager.isBluedotServiceInitialized()) {
 
-                mServiceManager.initialize(projectId, new InitializationResultListener() {
-                    @Override public void onInitializationFinished(@Nullable BDError bdError) {
-                        if (bdError != null) {
-                           logInfo("BD SDK Initialize Error "+bdError.getReason());
-                           return;
-                        }
+                mServiceManager.initialize(projectId, bdError -> {
+                    if (bdError != null) {
+                        logInfo("BD SDK Initialize Error " + bdError.getReason());
+                        return;
+                    }
 
-                        logInfo("BD SDK initialized");
+                    logInfo("BD SDK initialized");
 
-                        new GeoTriggeringService.GeoTriggerBuilder().notification(createNotification()).start(
-                                getApplicationContext(), new GeoTriggeringStatusListener() {
-                                    @Override public void onGeoTriggeringResult(@Nullable BDError bdError) {
+                    new GeoTriggeringService.GeoTriggerBuilder().notification(createNotification())
+                            .start(
+                                    getApplicationContext(), geoError -> {
 
-                                        if (bdError != null) {
-                                            logInfo("BD Geo-Trigger Error "+bdError.getReason());
+                                        if (geoError != null) {
+                                            logInfo("BD Geo-Trigger Error " + geoError.getReason());
                                             return;
                                         }
 
@@ -87,16 +85,14 @@ public class MainApplication extends Application implements MarketingCloudSdk.In
                                         metaData.put("ContactKey", salesforceContactKey);
                                         mServiceManager.setCustomEventMetaData(metaData);
 
-                                        logInfo("BD Geo-triggering started & Contact Key set as " + salesforceContactKey);
-                                    }
-                                });
-                    }
+                                        logInfo("BD Geo-triggering started & Contact Key set as "
+                                                        + salesforceContactKey);
+                                    });
                 });
             }
         } else {
             requestPermissions();
         }
-
     }
 
     private void requestPermissions() {
@@ -106,20 +102,18 @@ public class MainApplication extends Application implements MarketingCloudSdk.In
         startActivity(intent);
     }
 
-
     //=============================== [ Bluedot SDK end ] ===============================
-
 
     //=============================== [ Marketing Cloud SDK ]=================================
     private String salesforceContactKey;
-    private String app_id="";//Enter your App Id
-    private String access_token=""; //Enter Access Token
-    private String fcm_id ="";   //Enter FCM Id
-    private String mID = "";   // MID from Firebase setup
+    private final String app_id = "";//Enter your App Id
+    private final String access_token = ""; //Enter Access Token
+    private final String fcm_id = "";   //Enter FCM Id
+    private final String mID = "";   // MID from Firebase setup
 
     private void initCloudMobilePushSDK() {
 
-        MarketingCloudSdk.init((Context) this, MarketingCloudConfig.builder()
+        MarketingCloudSdk.init(this, MarketingCloudConfig.builder()
                 .setApplicationId(app_id)
                 .setAccessToken(access_token)
                 .setSenderId(fcm_id)
@@ -127,15 +121,8 @@ public class MainApplication extends Application implements MarketingCloudSdk.In
                 .setMid(mID)
                 .setNotificationCustomizationOptions(
                         NotificationCustomizationOptions.create(R.mipmap.ic_launcher, null,
-                                new com.salesforce.marketingcloud.notifications.NotificationManager.NotificationChannelIdProvider() {
-                                    @Override
-                                    @NonNull
-                                    public String getNotificationChannelId(@NonNull Context context,
-                                                                           @NonNull NotificationMessage notificationMessage) {
-                                        // Whatever custom logic required to determine which channel should be used for the message.
-                                        return CHANNEL_ID;
-                                    }
-                                })).build((Context) this), this);
+                                                                (context, notificationMessage) -> CHANNEL_ID))
+                .build(this), this);
     }
 
     @Override
@@ -143,34 +130,22 @@ public class MainApplication extends Application implements MarketingCloudSdk.In
 
         if (status.status() == InitializationStatus.Status.SUCCESS) {
             logInfo("Marketing Cloud SDK started");
-            salesforceContactKey = MarketingCloudSdk.getInstance().getRegistrationManager().getContactKey();
+            salesforceContactKey =
+                    MarketingCloudSdk.getInstance().getRegistrationManager().getContactKey();
             if (salesforceContactKey == null || salesforceContactKey.length() == 0) {
                 salesforceContactKey = UUID.randomUUID().toString();
-                MarketingCloudSdk.getInstance().getRegistrationManager().edit().setContactKey(salesforceContactKey).commit();
+                MarketingCloudSdk.getInstance()
+                        .getRegistrationManager()
+                        .edit()
+                        .setContactKey(salesforceContactKey)
+                        .commit();
             }
-
-        } else if (status.status() == InitializationStatus.Status.COMPLETED_WITH_DEGRADED_FUNCTIONALITY) {
-            // While the SDK is usable, something happened during init that you should address.
-            // This could include:
-
-            //Google play services encountered a recoverable error
-
-                /* The user had previously provided the location permission, but it has now been revoked.
-                 Geofence and Beacon messages have been disabled.  You will need to request the location
-                 permission again and re-enable Geofence and/or Beacon messaging again. */
-
-              /* Google Play Services attempted to update your SSL providers but failed.  It should be assumed that
-                  all network communications will fallback to TLS1.0.
-              */
         } else if (status.status() == InitializationStatus.Status.FAILED) {
             logInfo("Marketing Cloud SDK error: " + status.toString());
         } else {
             logInfo("Marketing Cloud SDK : Unknown error");
         }
-
-
     }
-
 
     //=============================== [ Marketing Cloud SDK end]=================================
 
@@ -181,7 +156,6 @@ public class MainApplication extends Application implements MarketingCloudSdk.In
         sendBroadcast(intent);
     }
 
-
     /**
      * Creates notification channel and notification, required for foreground service notification.
      *
@@ -191,32 +165,39 @@ public class MainApplication extends Application implements MarketingCloudSdk.In
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, android.app.NotificationManager.IMPORTANCE_LOW);
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                                            NotificationManager.IMPORTANCE_DEFAULT);
             notificationChannel.enableLights(false);
             notificationChannel.setLightColor(Color.RED);
             notificationChannel.enableVibration(false);
-            android.app.NotificationManager notificationManager = (android.app.NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+            android.app.NotificationManager notificationManager =
+                    (android.app.NotificationManager) this.getSystemService(
+                            Context.NOTIFICATION_SERVICE);
             notificationManager.createNotificationChannel(notificationChannel);
 
-            Notification.Builder notification = new Notification.Builder(getApplicationContext(), CHANNEL_ID)
-                    .setContentTitle(NOTIFICATION_TITLE)
-                    .setContentText(NOTIFICATION_CONTENT)
-                    .setStyle(new Notification.BigTextStyle().bigText(NOTIFICATION_CONTENT))
-                    .setOngoing(true)
-                    .setCategory(Notification.CATEGORY_SERVICE)
-                    .setSmallIcon(R.mipmap.ic_launcher);
+            Notification.Builder notification =
+                    new Notification.Builder(getApplicationContext(), CHANNEL_ID)
+                            .setContentTitle(NOTIFICATION_TITLE)
+                            .setContentText(NOTIFICATION_CONTENT)
+                            .setStyle(new Notification.BigTextStyle().bigText(NOTIFICATION_CONTENT))
+                            .setOngoing(true)
+                            .setCategory(Notification.CATEGORY_SERVICE)
+                            .setSmallIcon(R.mipmap.ic_launcher);
 
             return notification.build();
         } else {
 
-            NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext())
-                    .setContentTitle(NOTIFICATION_TITLE)
-                    .setContentText(NOTIFICATION_CONTENT)
-                    .setStyle(new NotificationCompat.BigTextStyle().bigText(NOTIFICATION_CONTENT))
-                    .setOngoing(true)
-                    .setCategory(Notification.CATEGORY_SERVICE)
-                    .setPriority(PRIORITY_MAX)
-                    .setSmallIcon(R.mipmap.ic_launcher);
+            NotificationCompat.Builder notification =
+                    new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                            .setContentTitle(NOTIFICATION_TITLE)
+                            .setContentText(NOTIFICATION_CONTENT)
+                            .setStyle(new NotificationCompat.BigTextStyle().bigText(
+                                    NOTIFICATION_CONTENT))
+                            .setOngoing(true)
+                            .setCategory(Notification.CATEGORY_SERVICE)
+                            .setPriority(PRIORITY_MAX)
+                            .setSmallIcon(R.mipmap.ic_launcher);
 
             return notification.build();
         }
